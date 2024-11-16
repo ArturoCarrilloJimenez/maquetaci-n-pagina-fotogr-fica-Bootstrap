37695069624b6215858e388d3c85dc604b121029
arturillo40 <arturojimenezalmontaras@gmail.com>
2024-11-16 20:50:37 +0100

    guardar en localStorage el tema

--- logic/index.js
+++ logic/index.js
@@ -1,4 +1,5 @@
 const temaOscuro = () => {
+    localStorage.setItem('tema', 'dark')
     document.querySelector('body').setAttribute('data-bs-theme', 'dark')
     document.querySelector('#icono-change-tema').setAttribute('class', 'bi bi-brightness-low-fill')
     document.querySelector('#logo').setAttribute('src', 'images/logo_blanco.png')
@@ -6,6 +7,7 @@ const temaOscuro = () => {
 }
 
 const temaClaro = () => {
+    localStorage.setItem('tema', 'light')
     document.querySelector('body').setAttribute('data-bs-theme', 'light')
     document.querySelector('#icono-change-tema').setAttribute('class', 'bi bi-moon-stars-fill')
     document.querySelector('#logo').setAttribute('src', 'images/logo.png')
@@ -15,3 +17,12 @@ const temaClaro = () => {
 const cambiarTema = () => {
     document.querySelector('body').getAttribute('data-bs-theme') === 'light' ? temaOscuro() : temaClaro()
 }
+
+const getTema = () => {
+    let tema = localStorage.getItem('tema')
+    if (tema === '') tema = 'light'
+
+    tema === 'light' ? temaClaro() : temaOscuro()
+}
+
+getTema()
